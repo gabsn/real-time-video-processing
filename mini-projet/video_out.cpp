2a65b97abf2a2b8c4568260f6c5ce83a58476bdb
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-17 19:09:14 +0200

    Mini-projet | Suppression du bug avec vref

--- mini-projet/video_out.cpp
+++ mini-projet/video_out.cpp
@@ -4,13 +4,15 @@
 #include <string>
 #include "video_out.h"
 
+#define WIDTH_3 (unsigned int)3*image->width
+
 using namespace std;
 
 void VIDEO_OUT::writer() {
     if(reset_n == false) {
         // Reset : on remet tous les paramètres à  zéro
         pixel_counter = 0;
-    } else if (href) {
+    } else if ((href && (vref && pixel_counter < WIDTH_3)) || (href && (!vref && pixel_counter >= WIDTH_3))) {
         // On écrit dans le tableau de pixels
         if (pixel_counter < SIZE)
             image->pixel[pixel_counter++] = pixel_in;
