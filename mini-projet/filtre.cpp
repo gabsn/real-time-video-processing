229b603e403591d9102bef06f3cd49287ebeef57
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-18 19:34:31 +0200

    wip

--- mini-projet/filtre.cpp
+++ mini-projet/filtre.cpp
@@ -4,21 +4,26 @@ void FILTRE::average() {
     if (!reset_n) {
         pixel_received_i = 0;
         pixel_computed_i = 0;
+        p_out = 0;
         h_out = false;
         v_out = false;
-    } else if (h_in && pixel_received_i < WIDTH+1) { // Première ligne de pixels + premier pixel de la première cologne
+    } else if (h_in && pixel_received_i < WIDTH+1 && v_in) { // Première ligne de pixels + premier pixel de la première cologne
         image[pixel_received_i++] = p_in;
+        p_out = 0;
         h_out = false;
         v_out = false;
-    } else if (h_in) { 
-        if (pixel_received_i == SIZE-1) {
+    } else if (h_in && pixel_received_i >= WIDTH+1) { // On peut calculer un p_out => h_out = true;
+        image[pixel_received_i] = p_in; 
+        if (pixel_received_i == SIZE-1) { // On a reçu le dernier pixel de l'image
             pixel_received_i = 0;
-        } else if (pixel_received_i < SIZE-1) {
-            image[pixel_received_i++] = p_in;
-            pixel_computed_i = pixel_received_i-(WIDTH+2);
-        } else if (pixel_computed_i < SIZE) {
-            image[pixel_received_i++] = p_in;
+        } else if (pixel_computed_i == SIZE-1) {
+            pixel_computed_i = 0;
+        } else if (pixel_received_i < SIZE-1 && pixel_received_i >= pixel_computed_i) {
+            pixel_computed_i = pixel_received_i-(WIDTH+1);
+            pixel_received_i++;
+        } else if (pixel_computed_i < SIZE-1 && pixel_received_i < pixel_computed_i) {
             pixel_computed_i++;
+            pixel_received_i++;
         } 
 
         if (pixel_computed_i == 0) // pixel en haut à gauche
@@ -33,7 +38,7 @@ void FILTRE::average() {
         {
             p_out = (image[(HEIGHT-2)*WIDTH] + image[(HEIGHT-2)*WIDTH+1] + image[(HEIGHT-1)*WIDTH] + image[(HEIGHT-1)*WIDTH+1])/4;
         }
-        else if (pixel_computed_i == HEIGHT*WIDTH-1) // pixel en bas à droite
+        else if (pixel_computed_i == SIZE-1) // pixel en bas à droite
         {
             p_out = (image[(HEIGHT-2)*WIDTH-2] + image[(HEIGHT-1)*WIDTH-1] + image[HEIGHT*WIDTH-2] + image[HEIGHT*WIDTH-1])/4;
         }
@@ -70,6 +75,10 @@ void FILTRE::average() {
             v_out = true;
         else 
             v_out = false;
+    } else {
+        p_out = 0;
+        h_out = false;
+        v_out = false;
     }
 }
 
