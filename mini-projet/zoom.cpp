5699048df74fd8c81187a65f0eae8f71b4c4125d
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-20 14:07:27 +0200

    wip

--- mini-projet/zoom.cpp
+++ mini-projet/zoom.cpp
@@ -2,7 +2,7 @@
 
 #define ACTIVE_RANGE ((i_in >= H4) && (i_in < H2+H4) && (j_in >= W4) && (j_in < W2+W4))
 
-void ZOOM::zoom_with_latency() {
+void ZOOM::zoom() {
     if (!reset_n) {
         nb_p_received = 0;
         nb_p_active = 0;
@@ -30,12 +30,12 @@ void ZOOM::zoom_with_latency() {
         nb_p_received++;
     }
 
-    /*****************************
+    /******************************
     * Gestion de l'image de sortie
-    ******************************/
+    *******************************/
     if (i_in > H4 && i_in <= H4+H2 && j_in == W4) { // On vient de récupérer une ligne de l'image que l'on veut afficher
         int i = i_in-(H4+1);
-        for (unsigned int j=0; j<W2; j++) { // On remplit un carré de 4 pixels conjoints à la même valeur
+        for (unsigned int j=0; j<W2; j++) { // On remplit un carré de 4 pixels conjoints à la même valeur (Vérifié)
             image_out[2*i*WIDTH+2*j] = image_received[i*W2+j];
             image_out[2*i*WIDTH+2*j+1] = image_received[i*W2+j];
             image_out[(2*i+1)*WIDTH+2*j] = image_received[i*W2+j];
@@ -48,6 +48,7 @@ void ZOOM::zoom_with_latency() {
      * Gestion des sorties
      **********************/
     if (new_line && (i_image_out < SIZE)) {
+        //cout << "(new_line && (i_image_out < SIZE)) = true : line_counter = " << line_counter << endl;
         if (line_counter > 0) {
             line_counter--;
         } else {
@@ -68,87 +69,6 @@ void ZOOM::zoom_with_latency() {
         p_out = 0;
         h_out = false;
         v_out = false;
-    }
-}
-
-void ZOOM::zoom() {
-    //cout << "entrée dans la méthode zoom" << endl;
-    if (!reset_n) {
-        //cout << "resetn activated" << endl;
-        pixel_counter = 0;
-        p_in_counter = 0;
-        p_out_counter = 0;
-        i_in = 0;
-        i_out = 0;
-        j_in = 0;
-        j_out = 0;
-        p_out = 0;
-        h_out = false;
-        v_out = false;
-    } else {
-        i_in = p_in_counter / WIDTH;
-        j_in = p_in_counter % WIDTH;
-        i_out = p_out_counter / W2;
-        j_out = p_out_counter % W2;
-        //cout << "p_in_counter = " << p_in_counter << " ";
-        //cout << "p_out_counter = " << p_out_counter << " " << endl;
-        //cout << "i_in = " << i_in << " ";
-        //cout << "j_in = " << j_in << " ";
-        //cout << "i_out = " << i_out << " ";
-        //cout << "j_out = " << j_out << endl;
-        //cout << "-----------------------------------" << endl;
-        
-        if (h_in && (i_in >= H4) && (i_in < H2+H4) && (j_in >= W4) && (j_in < W2+W4)) { // On est dans la zone de l'image qui correspond à un zoom x2
-            //cout << "W4 = " << W4 << endl;
-            //cout << "W2+W4 = " << W2+W4 << endl;
-            /******************************
-             * Réception de l'image zoomée 
-             ******************************/
-            //cout << "Image active :" << endl;
-            //cout << "p_in_counter = " << p_in_counter << " ";
-            //cout << "p_out_counter = " << p_out_counter << " " << endl;
-            //cout << "i_in = " << i_in << " ";
-            //cout << "j_in = " << j_in << " ";
-            //cout << "i_out = " << i_out << " ";
-            //cout << "j_out = " << j_out << endl;
-            //cout << "-----------------------------------" << endl;
- 
-            if (p_in_counter < SIZE) {
-                image_in[p_in_counter++] = p_in;
-                //cout << "p_in = " << (int)p_in << " & p_in_counter = " << p_in_counter << endl;
-
-                if (i_in > H4 && j_in == W4) { // On vient de récupérer une ligne de l'image que l'on veut afficher
-                    cout << "ligne récupérée" << endl;
-                    for (unsigned int j=0; j<W2; j+=2) { // On remplit un carré de 4 pixels conjoints à la même valeur
-                        image_out[i_out*WIDTH+j] = image_in[i_out*W2+j/2];
-                        image_out[i_out*WIDTH+j+1] = image_in[i_out*W2+j/2];
-                        image_out[(i_out+1)*WIDTH+j] = image_in[i_out*W2+j/2];
-                        image_out[(i_out+1)*WIDTH+j+1] = image_in[i_out*W2+j/2];
-                    }
-                    p_out_counter = p_out_counter + 2*WIDTH;
-                }
-            }
-
-            /**************************
-             * Envoi de l'image zoomée
-             **************************/
-            if (i_in > H4) {
-                p_out = image_out[pixel_counter++];
-                cout << "p_out = " << p_out << endl;
-                h_out = true;
-                v_out = (pixel_counter < 3*WIDTH) ? true : false;
-            } else {
-                p_out = 0;
-                h_out = false;
-                v_out = false;
-            }
-        } else { // On est en dehors de la zone de l'image qui nous intéresse
-            //cout << "En dehors de la partie de l'image qui nous intéresse" << endl;
-            p_in_counter++;
-            p_out = 0;
-            h_out = false;
-            v_out = false;
-        }
     }
 }
 
--- mini-projet/zoom.h
+++ mini-projet/zoom.h
@@ -24,26 +24,17 @@ SC_MODULE(ZOOM) {
      *  constructeur
      **************************************************/
     SC_CTOR(ZOOM) {
-        //zoom_with_latency()
+        new_line = false;
         line_counter = 0;
         nb_p_received = 0;
         nb_p_active = 0;
         i_image_out = 0;
-        new_line = false;
+        i_in = 0;
+        j_in = 0;
+        image_out = new unsigned char[SIZE];
         image_received = new unsigned char[H2*W2];
 
-        //zoom()
-        pixel_counter = 0;
-        p_in_counter = 0;
-        p_out_counter = 0;
-        i_in = 0;
-        i_out = 0;
-        j_in = 0;
-        j_out = 0;
-        image_in = new unsigned char[H2*W2];
-        image_out = new unsigned char[SIZE];
-
-        SC_METHOD(zoom_with_latency);
+        SC_METHOD(zoom);
         sensitive << clk.pos();
         async_reset_signal_is(reset_n,false);
     }
@@ -52,10 +43,8 @@ SC_MODULE(ZOOM) {
      *  méthodes et champs internes
      **************************************************/
     private:
-    void zoom_with_latency();
     void zoom();
 
-    // zoom_with_latency() attributes
     unsigned char * image_received;
     unsigned int nb_p_received;
     unsigned int nb_p_active;
@@ -63,16 +52,9 @@ SC_MODULE(ZOOM) {
     unsigned int line_counter;
     bool new_line;
 
-    // zoom() attributes
-    unsigned char * image_in;
     unsigned char * image_out;
-    unsigned int pixel_counter;
-    unsigned int p_in_counter;
-    unsigned int p_out_counter;
     unsigned int i_in;
     unsigned int j_in;
-    unsigned int i_out;
-    unsigned int j_out;
 };
 
 #endif
