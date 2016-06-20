c4158b806afc2699901b7d2a208d696070bbf256
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-20 15:48:49 +0200

    wip

--- mini-projet/video_out.cpp
+++ mini-projet/video_out.cpp
@@ -4,15 +4,13 @@
 #include <string>
 #include "video_out.h"
 
-#define WIDTH_3 (unsigned int)3*image->width
-
 using namespace std;
 
 void VIDEO_OUT::writer() {
     if(reset_n == false) {
         // Reset : on remet tous les paramètres à  zéro
         pixel_counter = 0;
-    } else if ((href && (vref && pixel_counter < WIDTH_3)) || (href && (!vref && pixel_counter >= WIDTH_3))) {
+    } else if ((href && (vref && pixel_counter < 3*W)) || (href && (!vref && pixel_counter >= 3*W))) {
         // On écrit dans le tableau de pixels
         if (pixel_counter < SIZE)
             image->pixel[pixel_counter++] = pixel_in;
