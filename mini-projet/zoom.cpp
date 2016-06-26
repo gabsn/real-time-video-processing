d7b5e017b536dd5851ccd4e14f01444bb41fcd60
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-26 17:34:52 +0200

    wip

--- mini-projet/zoom.cpp
+++ mini-projet/zoom.cpp
@@ -1,16 +1,10 @@
 #include "zoom.h"
 
 #define ACTIVE_RANGE ((i_in >= H4) && (i_in < H2+H4) && (j_in >= W4) && (j_in < W2+W4))
-#define W_MARGIN 154 
-#define H_MARGIN 49 
-#define WB 874
-#define HB 625
-#define SIZE_B 824*625
 
 void ZOOM::receiving() {
     if (!reset_n) {
         nb_p_received = 0;
-        nb_p_out = 0;
         start_sending = false;
     } 
 
@@ -19,7 +13,6 @@ void ZOOM::receiving() {
 
     if (h_in && ((v_in && nb_p_received < 3*W) || (!v_in && nb_p_received >= 3*W))) {
         nb_p_received = (nb_p_received == SIZE-1) ? 0 : nb_p_received+1;
-        nb_p_out = (nb_p_out == W2*H2-1) ? 0 : nb_p_out+1;
 
         if (ACTIVE_RANGE) {
             image_received[(i_in-H4)*W2+(j_in-W4)] = p_in;
--- mini-projet/zoom.h
+++ mini-projet/zoom.h
@@ -8,21 +8,11 @@
  *  définition du module
  **************************************/
 SC_MODULE(ZOOM) {
-    bool new_image;
-    bool restart;
+    // Public attributes
     sc_signal<bool> start_sending;
-    bool no_image_received;
     unsigned int nb_p_received;
-    unsigned int nb_p_tot;
-    unsigned int nb_p_out;
-    unsigned int nb_p_active;
-    unsigned int margin;
     unsigned int i_in;
     unsigned int j_in;
-    unsigned int i_tot;
-    unsigned int j_tot;
-    unsigned int i_out;
-    unsigned int j_out;
 
     // IO PORTS
     sc_in<bool>          clk;
@@ -39,21 +29,9 @@ SC_MODULE(ZOOM) {
      *  constructeur
      **************************************************/
     SC_CTOR(ZOOM) {
-        new_image = false;
-        restart = true;
         start_sending = false;
-        no_image_received = true;
         nb_p_received  = 0;
-        nb_p_active    = 0;
-        nb_p_out    = 0;
-        nb_p_tot    = 0;
-        i_out = 0;
-        j_out = 0;
-        i_image_out    = 0;
-        count          = 0;
-        total_count    = 0;
         image_received = new unsigned char[H2*W2];
-        image_out      = new unsigned char[SIZE];
 
         SC_METHOD(receiving);
         sensitive << clk.pos();
@@ -68,11 +46,7 @@ SC_MODULE(ZOOM) {
     void receiving();
     void sending();
 
-    unsigned int i_image_out;
-    unsigned int count;
-    unsigned int total_count;
     unsigned char * image_received;
-    unsigned char * image_out;
 };
 
 #endif
