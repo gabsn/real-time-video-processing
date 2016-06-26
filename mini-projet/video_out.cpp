e4e88f9aae4c0f047ab6462ac3500c4e1fd29071
Gabin Marignier <gabin.marignier@gmail.com>
2016-06-26 18:02:44 +0200

    COMMIT FINAL

--- mini-projet/video_out.cpp
+++ mini-projet/video_out.cpp
@@ -13,18 +13,18 @@ void VIDEO_OUT::writer() {
     } 
     
     if ((href && (vref && pixel_counter < 3*W)) || (href && (!vref && pixel_counter >= 3*W))) {
-        if (pixel_counter < SIZE)
+        if (pixel_counter < SIZE-1)
             image->pixel[pixel_counter++] = pixel_in;
-        else
-            cout << "ERROR: pixel_counter > SIZE" << endl;
+        else {
+            image->pixel[pixel_counter] = pixel_in;
+            pixel_counter = 0;
 
-        if (pixel_counter == SIZE) {
+            // Ecriture du fichier .png
             ostringstream convert;
             convert << image_counter;
             string number = convert.str(); 
             string filename = "wallace_out_"+number;
             image_write(image,filename.c_str());
-            pixel_counter = 0;
             image_counter++;
         }
     }
